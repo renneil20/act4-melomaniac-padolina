7dcca9905958fbf4a18a5e64d5aa3f85faf28327
PadolinaReinelle <reinellepadolina@gmail.com>
2022-05-21 18:42:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="images/logo-img.png" height="30" width="30">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <a class="navbar-brand" href="#page-top"> THS Playlist </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <body background="images/back_img.jpg" >
+            <div class="container px-4 text-center">
+                <img src="images/logo-img.png" height="100" width="100">
+                <h1 class="fw-bolder">Welcome to THS Playlist</h1>
+                <p class="lead">All songs are for people who expect to be loved and hurt because of love</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     <h1 class="text-primary">SONG LIST</h1>
+                        <br/>
+                        <div class="py-7 px-9">
+                        <li><h5 class='fw-bolder'>Song 1 - "Hindi Tayo Pwede"</h7><i> -  The Juans</i></li>
+                        <li><h5 class='fw-bolder'>Song 2 - "Paubaya"</h7><i> - Moira Dela Torre</i></li>
+                        <li><h5 class='fw-bolder'>Song 3 - "Di lang Ikaw"        </h7><i> - Juris Fernandez</i></li>
+                        <li><h5 class='fw-bolder'>Song 4 - "Pano"</h7><i> - Zack Tabudlo</i></li>
+                        <li><h5 class='fw-bolder'>Song 5 - "Imahe"</h7><i> - Magnus Haven</i></li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Reinelle P. Padolina</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ song1.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="images/logo-img.png" height="30" width="30">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <a class="navbar-brand" href="#page-top"> THS Playlist </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+               <img src="images/song1_img.jpg" width="350" height="250">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <h1 class="fw-bolder">Hindi Tayo Pwede</h1>
+                <p class="lead">The Juans</p>
+                <br>
+                    <audio controls>
+                    <source src="music/juans-hindi-tayo-pwede.mp3" type="audio/mpeg">
+                    </audio>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Kathang Isip</h2>";
+                        echo "<p class='lead '>Ben & Ben</p>";
+                
+                        echo "<p> Diba nga ito ang iyong gusto? </p>";
+                        echo "<p> O,Ito'y lilisan na ako</p>";
+                        echo "<p> Mga alaala'y ibabaon</p>";
+                        echo "<p> Kalakip ang tamis ng kahapon</p>";
+                        echo "<br/>";
+
+                        echo "<p> Mga gabing di namamalayang</p>";
+                        echo "<p> Oras ay lumilipad</p>";
+                        echo "<p> Mga sandaling lumalayag kung</p>";
+                        echo "<p> San man tayo mapadpad</p>";
+                        echo "<p> Bawat Kilig na nadarama</p>";
+                        echo "<p> Sa tuwing hawak ang 'yong kamay</p>";
+                        echo "<p> Ito'y maling akala</p>";
+                        echo "<p> Isang malaking sablay</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pasensya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kung gaano kabilis nagsimula</p>";
+                        echo "<p> Gano'n katulin nawala</p>";
+                        echo "<p> Maaari ba tayong bumalik sa umpisa</p>";
+                        echo "<p> Upang di na umasa ang pusong nagiisa</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pasensiya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sumabay sa agos na isinulat ng tadhana</p>";
+                        echo "<p> Minsan siya'y para sa iyo</p>";
+                        echo "<p> Pero minsan siya'y paasa</p>";
+                        echo "<p> Tatakbo papalayo't</p>";
+                        echo "<p> Kakalimutan ang lahat</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pero kahit saan man lumingon</p>";
+                        echo "<p> Nasusulyapan ang kahapon</p>";
+                        echo "<p> At sa aking bawat paghinga</p>";
+                        echo "<p> Ikaw ang nasa isip ko, sinta</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kaya pasensya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Mula sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Diba nga ito ang iyong gusto?</p>";
+                        echo "<p> O,Ito'y lilisan na ako</p>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Reinelle P. Padolina</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
