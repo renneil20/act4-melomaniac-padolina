83e798acc4c3ab2de8a1291edb35e94e5d1391df
PadolinaReinelle <reinellepadolina@gmail.com>
2022-05-22 12:59:24 +0700

    song 4

---
+++ song4.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="images/logo-img.png" height="30" width="30">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <a class="navbar-brand" href="#page-top"> THS Playlist </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+               <img src="images/song4_img.jpg" width="350" height="250">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <h1 class="fw-bolder">Pano</h1>
+                <p class="lead">Zack Tabudlo</p>
+                <br>
+                    <audio controls>
+                    <source src="music/zack-pano.mp3" type="audio/mpeg">
+                    </audio>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Pano</h2>";
+                        echo "<p class='lead '>Zack Tabudlo</p>";
+                
+                        echo "<p>                C             </p>";
+                        echo "<p> Oh giliw naririnig mo ba </p>";
+                        echo "<p>           Cmaj7       </p>";
+                        echo "<p> Ang yong sarili </p>";
+                        echo "<p>               C7         </p>";
+                        echo "<p> Nakakabaliw lumabas  </p>";
+                        echo "<p>               Dm7           </p>";
+                        echo "<p> Sa yong bibig  </p>";
+                        echo "<p> C   <p/>";
+                        echo "<p> Alam kong uto uto ako  </p>";
+                        echo "<p>        Cmaj7         <p/>";
+                        echo "<p> Alam ko na marupok  </p>";
+                        echo "<p>       C7                  Dm7         </p>";
+                        echo "<p> Tao lang din naman kasi ako  </p>";
+                        echo "<br/>";
+
+                        echo "<p> Am7                   G          </p>";
+                        echo "<p> May nararamdaman din ako </p>";
+                        echo "<p>       F           Fm7    </p>";
+                        echo "<p> Di kasi manhid na tulad mo  </p>";
+                        echo "<p>               Am7          </p>";
+                        echo "<p> Alam kong sanay bumitaw </p>";
+                        echo "<p>             G         </p>";
+                        echo "<p> Ang isang tulad mo </p> ";
+                        echo "<p>       F           Fm7       </p>";
+                        echo "<p> Lalayo na ba ako </p>";
+                        echo "<br/>";
+
+                        echo "<p>               C               </p>";
+                        echo "<p> Pano nanaman ako  </p>";
+                        echo "<p>              Cmaj7         </p>";
+                        echo "<p> Nahulog Na sayo   </p>";
+                        echo "<p> C7                Dm7    </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako   </p>";
+                        echo "<p>                C            </p> ";
+                        echo "<p>  Pano naman ako </p>";
+                        echo "<p>                Cmaj7           </p>";
+                        echo "<p>   Naghihitay ng matagal sayo  </p>";
+                        echo "<p>       C7                             Dm7       </p>";
+                        echo "<p>  Wala lang ba talaga lahat ng yon sayo</p>";
+                        echo "<p>   C       Cmaj7       C7          Dm7  </p> ";
+                        echo "<p> Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+                        echo "<p>                C               </p>";
+                        echo "<p> Sinasadya mo ba ang lahat  </p> ";
+                        echo "<p>      Cmaj7      </p>";
+                        echo "<p> O trip mo lang ba ako saktan </p>";
+                        echo "<p>           C7          </p>";
+                        echo "<p> Pagtapos kong ibigay balikat ko  </p>";
+                        echo "<p>             Dm7          </p>";
+                        echo "<p> Pag ika'y umiiyak </p>";
+                        echo "<p> C </p>";
+                        echo "<p> anong bang tingin mo sakin </p>";
+                        echo "<p> Cmaj7  </p>";
+                        echo "<p> Iisa ba akong alipin </p>";
+                        echo "<p> C7 </p>";
+                        echo "<p> wala ka bang modo </p>";
+                        echo "<p> anong ginawa mo </p> ";
+                        echo "<p> Dm7 </p>";
+                        echo "<p> Nagtiwala  naman sayo </p>";
+                        echo "<br/>";
+
+                        echo "<p> Am7                   G          </p>";
+                        echo "<p> May nararamdaman din ako </p>";
+                        echo "<p>       F           Fm7    </p>";
+                        echo "<p> Di kasi manhid na tulad mo  </p>";
+                        echo "<p>               Am7          </p>";
+                        echo "<p> Alam kong sanay bumitaw </p>";
+                        echo "<p>             G         </p>";
+                        echo "<p> Ang isang tulad mo </p> ";
+                        echo "<p>       F           Fm7       </p>";
+                        echo "<p> Lalayo na ba ako </p>";
+                        echo "<br/>";
+
+                        echo "<p>               C               </p>";
+                        echo "<p> Pano nanaman ako  </p>";
+                        echo "<p>              Cmaj7         </p>";
+                        echo "<p> Nahulog Na sayo   </p>";
+                        echo "<p> C7                Dm7    </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako   </p>";
+                        echo "<p>                C            </p> ";
+                        echo "<p>  Pano naman ako </p>";
+                        echo "<p>                Cmaj7           </p>";
+                        echo "<p>   Naghihitay ng matagal sayo  </p>";
+                        echo "<p>       C7                             Dm7       </p>";
+                        echo "<p>  Wala lang ba talaga lahat ng yon sayo</p>";
+                        echo "<p>   C       Cmaj7       C7          Dm7  </p> ";
+                        echo "<p> Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+                        echo "<p>               C               </p>";
+                        echo "<p> Pano nanaman ako  </p>";
+                        echo "<p>              Cmaj7         </p>";
+                        echo "<p> Nahulog Na sayo   </p>";
+                        echo "<p> C7                Dm7    </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako   </p>";
+                        echo "<p>                C            </p> ";
+                        echo "<p>  Pano naman ako </p>";
+                        echo "<p>                Cmaj7           </p>";
+                        echo "<p>   Naghihitay ng matagal sayo  </p>";
+                        echo "<p>       C7                             Dm7       </p>";
+                        echo "<p>  Wala lang ba talaga lahat ng yon sayo</p>";
+                        echo "<p>   C       Cmaj7       C7          Dm7  </p> ";
+                        echo "<p> Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Reinelle P. Padolina</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
