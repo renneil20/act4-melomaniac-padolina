7c548d39d8fee7de9a06afed1b83303b910a6afb
PadolinaReinelle <reinellepadolina@gmail.com>
2022-05-22 10:16:07 +0700

    song2

---
+++ song2.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="images/logo-img.png" height="30" width="30">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <a class="navbar-brand" href="#page-top"> THS Playlist </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+               <img src="images/song2_img.jpg" width="350" height="250">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <h1 class="fw-bolder">Paubaya</h1>
+                <p class="lead">Moira Dela Torre</p>
+                <br>
+                    <audio controls>
+                    <source src="music/paubaya-moira.mp3" type="audio/mpeg">
+                    </audio>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Paubaya</h2>";
+                        echo "<p class='lead '>Moira Dela Torre</p>";
+                
+                        echo "<p>   F         Am        B            C     </p>";
+                        echo "<p> Saan nagsimula magbago ang lahat </p>";
+                        echo "<p>   F             Am                Bb     C    </p>";
+                        echo "<p> Kailan, nung akoy'y di na naging sapat </p>";
+                        echo "<p>   Bb        Bbm       Am          Dm  </p>";
+                        echo "<p> Ba't di mo sinabi nung una pa lang </p>";
+                        echo "<p>   Gm       Am     Bb         C       </p>";
+                        echo "<p> Ako ang kailangan, pero 'di ang mahal </p>";
+                        echo "<br>";
+
+                        echo "<p> F     Am          Bb        C       </p>";
+                        echo "<p> Saan nagkulang ang aking pagmamahal?  </p>";
+                        echo "<p> F         Am          Bb          C    </p>";
+                        echo "<p> Lahat ay binigay, nang mapangiti ka lang  </p>";
+                        echo "<p>  Bb       Bbm      Am       Dm     </p>";
+                        echo "<p> Ba't Di ko nakita na ayaw mo na? </p>";
+                        echo "<p> Gm        Am           Bb         C       F  </p>";
+                        echo "<p> Ako ang kasama, pero hanap, mo'y siya ";
+                        echo "<br/>";
+
+                        echo "<p> Bb        C       Am       Dm      </p>";
+                        echo "<p> At kung masaya ka sa piling niya  </p>";
+                        echo "<p>  Gm                   C     </p>";
+                        echo "<p> Hindi ko na pipilit  pa  </p>";
+                        echo "<p>  Bb       C          Am       Dm      </p>";
+                        echo "<p> Ang tanging hiling ko lang sakanya  </p>";
+                        echo "<p> Gm        Am    Bb           C        F   <p/> ";
+                        echo "<p> Wag kang paluhain, at alagaan ka niya  </p>";
+                        echo "<br/>";
+
+                        echo "<p> F     Am     Bb      C    </p>";
+                        echo "<p> Saan natigil ang pagiging totoo </p>";
+                        echo "<p>  F            Am           Bb       <p/>";
+                        echo "<p> Sa tuwing mababanggit na mahal mo ako </p>";
+                        echo "<p> Bb        Bbm         Am       Dm     </p>";
+                        echo "<p> Ba't di mo inamin na merong iba?</p>";
+                        echo "<p> Gm        Am  Bb      C       F  </p>";
+                        echo "<p> Ako ang kayakap, pero isip mo'y siya  </p>";
+                        echo " <br/>";
+
+                        echo "<p> Bb        Bbm       Am       Dm      </p>";
+                        echo "<p> Ba't di ko naisip na merong hanggan  </p>";
+                        echo "<p>  Gm       Am       Bb       </p>";
+                        echo "<p> Ako yung nauna  </p>";
+                        echo "<p>   C              F            </p>";
+                        echo "<p> pero siya ang wakas   </p>";
+                        echo "<br/>";
+
+
+                        echo "<p>   Bb          C          Am    Dm      </p>";
+                        echo "<p>   At kita naman  sa 'yong  mga mata  </p>";
+                        echo "<p>   Gm          C       </p>";
+                        echo "<p>   Kung bakit pinili mo siya </p>";
+                        echo "<p>   Bb      C     Am      Dm     </p>";
+                        echo "<p>   Mahirap labanan ang tinadhana </p>";
+                        echo "<p>   Gm      Am  Bb Am  </p>";
+                        echo "<p>   Pinapaubaya, </p>";
+                        echo "<p>   Gm      Am  Bb Am  </p>";
+                        echo "<p>   Pinapaubaya, </p>";
+                        echo "<p>   Gm   Am  Bb  C   F  <p/>";
+                        echo "<p>   Pinapaubaya, ko na sa kanya <p/>";
+                        echo "<br/>";
+                      
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Reinelle P. Padolina</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
