66a1f047258f3af877d05eba4958fab6749af66f
PadolinaReinelle <reinellepadolina@gmail.com>
2022-05-22 14:14:42 +0700

    song5

---
+++ song5.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="images/logo-img.png" height="30" width="30">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <a class="navbar-brand" href="#page-top"> THS Playlist </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+               <img src="images/song5_img.jpg" width="350" height="250">
+                <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link text-light" href="#"></a></li>
+                </ul>
+                <h1 class="fw-bolder">Imahe</h1>
+                <p class="lead">Magnus Haven</p>
+                <br>
+                    <audio controls>
+                    <source src="music/imahe-magnus.mp3" type="audio/mpeg">
+                    </audio>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Imahe</h2>";
+                        echo "<p class='lead '>Magnus Haven</p>";
+                
+                        echo "<p>      Dm7            C#m           </p>";
+                        echo "<p> Kinukulayan ang isipan  </p>";
+                        echo "<p>          C#m      </p>";
+                        echo "<p> Pabalik sa nakaraan  </p>";
+                        echo "<p>        Dm7       </p>";
+                        echo "<p>'Wag mo ng  balikan </p>";
+                        echo "<p>             C#m          </p>";
+                        echo "<p> Patuloy ka lang masasaktan   </p>";
+
+                        echo "<p> Dm7  <p/>";
+                        echo "<p> Hindi nagkulang kakaisip </p>";
+                        echo "<p>       C#m      <p/>";
+                        echo "<p> Sa isang magandang larawan   </p>";
+                        echo "<p>             Dm7         </p>";
+                        echo "<p> Paulitulit na binabanggit  </p>";
+                        echo "<p>               C#m        </p>";
+                        echo "<p> Ang  pangalang nakasanayan </p>";
+                        echo "<br/>";
+
+                        echo "<p> Bm </p>";
+                        echo "<p> Tayo ay pinagtagpo </p>";
+                        echo "<p> C#m </p>";
+                        echo "<p> Ngunit hindi tinadhana  </p>";
+                        echo "<p> Bm            C#m     </p>";
+                        echo "<p> Sadyang mapaglaro itong mundo </p>";
+                        echo "<br/>";
+
+
+                        echo "<p> Dm7       C#m     </p>";
+                        echo "<p> Kinalimutan  kahit nahihirapan </p>";
+                        echo "<p> Dm7       C#m  </p> ";
+                        echo "<p> Para sa sariling kapakanan </p>";
+                        echo "<p> Dm7        C#m         </p>";
+                        echo "<p> kinalimutan kahit nahihirapan </p>";
+                        echo "<p> Dm7           C#m  </p>";
+                        echo "<p> Mga oras na hindi na mababalikan  </p>";
+                        echo "<br/>";
+
+                        echo "<p>               Bm              </p>";
+                        echo "<p> Pinagtagpo  </p>";
+                        echo "<p>              C#m        </p>";
+                        echo "<p> Ngunit hindi tinadhana  </p>";
+                        echo "<p> Bm            C#m    </p>";
+                        echo "<p> Puso natin ay hindi   </p>";
+                        echo "<p>   E  </p> ";
+                        echo "<p>  Sa isa't isa </p>";
+                        echo "<br/>";
+
+                        echo "<p>  Dm7   </p>";
+                        echo "<p> Hindi na maibabalik ang dati nating pagsasama  </p>";
+                        echo "<p>   C#m   </p>";
+                        echo "<p> Ang tamis ng iyong halik ay di na madarama </p>";
+                        echo "<p>     Dm7  </p> ";
+                        echo "<p> Pangako sa isa't isa ay di na mabubuhay pa </p>";
+                        echo "<p> C#m </p>";
+                        echo "<p> Paalam sa 'ting pag ibig na minsa'y pinag-isa </p>";
+                        echo "<br/>";
+
+                        echo "<p> Bm </p>";
+                        echo "<p> Tayo ay pinagtagpo </p>";
+                        echo "<p> C#m </p>";
+                        echo "<p> Ngunit hindi tinadhana  </p>";
+                        echo "<p> Bm            C#m     </p>";
+                        echo "<p> Sadyang mapaglaro itong mundo </p>";
+                        echo "<br/>";
+
+
+                        echo "<p> Dm7       C#m     </p>";
+                        echo "<p> Kinalimutan  kahit nahihirapan </p>";
+                        echo "<p> Dm7       C#m  </p> ";
+                        echo "<p> Para sa sariling kapakanan </p>";
+                        echo "<p> Dm7        C#m         </p>";
+                        echo "<p> kinalimutan kahit nahihirapan </p>";
+                        echo "<p> Dm7           C#m  </p>";
+                        echo "<p> Mga oras na hindi na mababalikan  </p>";
+                        echo "<br/>";
+
+                        echo "<p>               Bm              </p>";
+                        echo "<p> Pinagtagpo  </p>";
+                        echo "<p>              C#m        </p>";
+                        echo "<p> Ngunit hindi tinadhana  </p>";
+                        echo "<p> Bm            C#m    </p>";
+                        echo "<p> Puso natin ay hindi   </p>";
+                        echo "<p>   E  </p> ";
+                        echo "<p>  Sa isa't isa </p>";
+                        echo "<br/>";
+
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Reinelle P. Padolina</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
